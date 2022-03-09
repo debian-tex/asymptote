e8c3da7edcf6e552e32b1d89504a1157c8fe3834
Hilmar Preusse <hille42@web.de>
2022-03-09 10:19:57 +0100

    New upstream version 2.79+ds

--- LspCpp/CMakeFiles/lspcpp.dir/DependInfo.cmake
+++ LspCpp/CMakeFiles/lspcpp.dir/DependInfo.cmake
@@ -8,31 +8,31 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/Context.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/Context.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/Context.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/Endpoint.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/Endpoint.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/Endpoint.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/GCThreadContext.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/GCThreadContext.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/GCThreadContext.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/MessageJsonHandler.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/MessageJsonHandler.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/MessageJsonHandler.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/RemoteEndPoint.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/RemoteEndPoint.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/RemoteEndPoint.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/StreamMessageProducer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/StreamMessageProducer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/StreamMessageProducer.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/TcpServer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/TcpServer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/TcpServer.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/WebSocketServer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/WebSocketServer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/WebSocketServer.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/message.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/message.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/message.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/serializer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/serializer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/serializer.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/jsonrpc/threaded_queue.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/threaded_queue.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/threaded_queue.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/Markup.cpp" "CMakeFiles/lspcpp.dir/src/lsp/Markup.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/Markup.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/ParentProcessWatcher.cpp" "CMakeFiles/lspcpp.dir/src/lsp/ParentProcessWatcher.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/ParentProcessWatcher.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/ProtocolJsonHandler.cpp" "CMakeFiles/lspcpp.dir/src/lsp/ProtocolJsonHandler.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/ProtocolJsonHandler.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/initialize.cpp" "CMakeFiles/lspcpp.dir/src/lsp/initialize.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/initialize.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/lsp.cpp" "CMakeFiles/lspcpp.dir/src/lsp/lsp.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/lsp.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/lsp_diagnostic.cpp" "CMakeFiles/lspcpp.dir/src/lsp/lsp_diagnostic.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/lsp_diagnostic.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/textDocument.cpp" "CMakeFiles/lspcpp.dir/src/lsp/textDocument.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/textDocument.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/utils.cpp" "CMakeFiles/lspcpp.dir/src/lsp/utils.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/utils.cpp.o.d"
-  "/usr/local/src/asymptote-2.78/LspCpp/src/lsp/working_files.cpp" "CMakeFiles/lspcpp.dir/src/lsp/working_files.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/working_files.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/Context.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/Context.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/Context.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/Endpoint.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/Endpoint.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/Endpoint.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/GCThreadContext.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/GCThreadContext.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/GCThreadContext.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/MessageJsonHandler.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/MessageJsonHandler.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/MessageJsonHandler.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/RemoteEndPoint.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/RemoteEndPoint.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/RemoteEndPoint.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/StreamMessageProducer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/StreamMessageProducer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/StreamMessageProducer.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/TcpServer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/TcpServer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/TcpServer.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/WebSocketServer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/WebSocketServer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/WebSocketServer.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/message.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/message.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/message.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/serializer.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/serializer.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/serializer.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/jsonrpc/threaded_queue.cpp" "CMakeFiles/lspcpp.dir/src/jsonrpc/threaded_queue.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/jsonrpc/threaded_queue.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/Markup.cpp" "CMakeFiles/lspcpp.dir/src/lsp/Markup.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/Markup.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/ParentProcessWatcher.cpp" "CMakeFiles/lspcpp.dir/src/lsp/ParentProcessWatcher.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/ParentProcessWatcher.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/ProtocolJsonHandler.cpp" "CMakeFiles/lspcpp.dir/src/lsp/ProtocolJsonHandler.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/ProtocolJsonHandler.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/initialize.cpp" "CMakeFiles/lspcpp.dir/src/lsp/initialize.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/initialize.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/lsp.cpp" "CMakeFiles/lspcpp.dir/src/lsp/lsp.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/lsp.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/lsp_diagnostic.cpp" "CMakeFiles/lspcpp.dir/src/lsp/lsp_diagnostic.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/lsp_diagnostic.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/textDocument.cpp" "CMakeFiles/lspcpp.dir/src/lsp/textDocument.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/textDocument.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/utils.cpp" "CMakeFiles/lspcpp.dir/src/lsp/utils.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/utils.cpp.o.d"
+  "/usr/local/src/asymptote-2.79/LspCpp/src/lsp/working_files.cpp" "CMakeFiles/lspcpp.dir/src/lsp/working_files.cpp.o" "gcc" "CMakeFiles/lspcpp.dir/src/lsp/working_files.cpp.o.d"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/usr/local/src/asymptote-2.78/LspCpp/third_party/uri/src/CMakeFiles/network-uri.dir/DependInfo.cmake"
+  "/usr/local/src/asymptote-2.79/LspCpp/third_party/uri/src/CMakeFiles/network-uri.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
