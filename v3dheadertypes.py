451135e952e7deb959e55dfa1c507989f717611c
Hilmar Preuße <hille42@web.de>
2024-03-28 23:02:45 +0100

    New upstream version 2.89+ds

--- v3dheadertypes.py
+++ v3dheadertypes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Enum class for v3dheadertypes
 """ AUTO-GENERATED from v3dheadertypes.csv """
-# Generated at 2024-03-08 08:14:52
+# Generated at 2024-03-23 21:31:00
 
 class v3dheadertypes:
     v3dheadertypes_canvasWidth=1
